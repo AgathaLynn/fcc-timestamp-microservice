62417121f7de930de95cf3bae9ce4f1a2ff6c8d5
AgathaLynn <amaskelton@verizon.net>
2016-01-24 20:02:33 +0000

    add date-formatting functionality

---
+++ app/controllers/dateFormatter.js
@@ -0,0 +1,37 @@
+'use strict';
+var exports = module.exports = {};
+
+exports.build_json = function(url_component) {
+    
+    var date = new Date(url_component);
+    var to_return = {};
+    
+    function formatDate(date_obj) {
+    
+        var month_array = ['January', 'February', 'March', 'April', 
+                        'May', 'June', 'July', 'August', 
+                        'September', 'October', 'November', 'December'];
+                        
+        var day = date_obj.getDate();
+        var month = month_array[date_obj.getMonth()];
+        var year = date_obj.getFullYear();
+    
+        return month + ' ' + String(day) + ', ' + String(year);
+    } 
+    
+    if (date == 'Invalid Date') {
+        to_return.unix = null;
+        to_return.natural = null;
+    } else {
+        to_return.unix = Date.parse(date);
+        to_return.natural = formatDate(date);
+    }
+    
+    return JSON.stringify(to_return);
+    
+}
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 'use strict';
 
 var express = require('express');
+var formatter = require(process.cwd() + '/app/controllers/dateFormatter.js');
 
 var app = express();
 
 app.get('/*', function(req, res) {
-    res.sendFile(process.cwd() + '/public/index.html');
-    console.log(req.url);
+    var date_component = decodeURIComponent(req.url.slice(1));
+    
+    if (date_component == '') {
+        
+        res.sendFile(process.cwd() + '/public/index.html');
+        
+    } else {
+        
+        var date_json = formatter.build_json(date_component);
+        res.send(date_json);
+        
+    }
 });
 
 app.listen(8080, function() {
