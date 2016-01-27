a3b30561e72f9ea0049c7b6497ec00f925db112f
AgathaLynn <amaskelton@verizon.net>
2016-01-27 19:33:14 +0000

    change port for Heroku

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.get('/*', function(req, res) {
     }
 });
 
-app.listen(8080, function() {
+app.listen(process.env.PORT || 8080, function() {
     console.log('Listening on port 8080...');
 });
